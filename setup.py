5a479aaf7e7cc1203dbcc28fe11e6038e6b04b42
Chandler Hagan <gchagan36@gmail.com>
2024-02-09 22:27:52 -0500

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+
+from setuptools import find_packages, setup
+
+VERSION = "0.1.0"
+setup(name="pysportradar",
+      description="Wrapper for SportRadar APIs",
+      license="GNU",
+      author="Chandler Hagan",
+      packages=find_packages(exclude=["test"]),
+      install_requires=["requests"],
+      python_requires=">=3.12.1"
+)
